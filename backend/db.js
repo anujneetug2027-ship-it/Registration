8566c201aa1656981b97875276a4e8cf2ef93336
anujneetug2027-ship-it <anujneetug2027@gmail.com>
2025-10-25 00:50:39 +0530

    Fix MongoDB URI encoding and formatting

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://ambikashelf:anuj#678@cluster0.pulil65.mongodb.net/ambikashelfDB?retryWrites=true&w=majority";
+const uri = "mongodb+srv://ambikashelf:anuj%23678@cluster0.pulil65.mongodb.net/ambikashelfDB?retryWrites=true&w=majority";
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
-        useUnifiedTopology: true
-        })
-        .then(() => console.log('MongoDB Connected!'))
-        .catch(err => console.log('MongoDB connection error:', err));
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected!'))
+.catch(err => console.log('MongoDB connection error:', err));
 
-        module.exports = mongoose;
+module.exports = mongoose;
